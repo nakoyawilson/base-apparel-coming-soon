b461ab7004cf8004d7b23bca194d36114d38a1c7
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-12-13 11:00:37 -0400

    Amend form styles and functionality

--- js/main.js
+++ js/main.js
@@ -1,26 +1,27 @@
 const submit = document.querySelector(".submit");
 const inputField = document.querySelector("input");
 const errorMessage = document.querySelector(".error-message");
-const errorIcon = document.querySelector(".error-icon");
+const errorIcon = document.querySelector(".icon");
+const form = document.querySelector(".form-content");
 const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 submit.addEventListener("click", (e) => {
   emailAddress = inputField.value;
   if (emailAddress === "") {
-    errorIcon.style.display = "block";
+    errorIcon.style.opacity = "1";
     errorMessage.style.display = "block";
     errorMessage.innerHTML =
       "Whoops! It looks like you forgot to add your email";
-    inputField.style.border = "2px solid hsl(0, 93%, 68%)";
+    form.style.border = "2px solid hsl(0, 93%, 68%)";
   } else if (!emailAddress.match(emailRegex)) {
-    errorIcon.style.display = "block";
+    errorIcon.style.opacity = "1";
     errorMessage.style.display = "block";
     errorMessage.innerHTML = "Please provide a valid email";
-    inputField.style.border = "2px solid hsl(0, 93%, 68%)";
+    form.style.border = "2px solid hsl(0, 93%, 68%)";
   } else {
-    errorIcon.style.display = "none";
+    errorIcon.style.opacity = "0";
     errorMessage.style.display = "none";
-    inputField.style.border = "1px solid hsla(0, 6%, 24%, 0.2)";
+    form.style.border = "1px solid hsla(0, 6%, 24%, 0.2)";
     inputField.value = "";
   }
   e.preventDefault();
